73b6df883d10acf174962d9901767da4dbd6b2f4
Stephen <bubalicio@gmail.com>
2012-08-30 23:06:45 -0700

    Finsihed off simple compare.

--- Dircompare.py
+++ Dircompare.py
@@ -46,15 +46,23 @@ def setFiledata():
             
 
     #temp set of dir so I don't have to enter it every time
-    fdir = "F:\\Python27\\Test"
-    
+    fdir = "C:\\Python27\\Test"
+
+    #function to map out save file for direcotry
     dSearch(fdir, f)
 
+    #function compare files
+    #using premade data for checking.
+    filetemp1 = file("test.txt", "r+")
+    filetemp2 = file("test1.txt", "r+")
+    dCompare(filetemp1, filetemp2)
+
         
-#function to check directory for files(recursive)
+#function to check directory for files
+#recursive
 def dSearch(filedir, fsave):
-
-
+    print "in dSearch"
+    
     #prints out size of file in an easier to read format
     for filename in os.listdir(filedir):
         print filename
@@ -71,6 +79,7 @@ def dSearch(filedir, fsave):
 
         #check if the current file is a directory of file
         #output debug info to make sure working correctly
+        #TODO possible add complete filepath for easier checking of files (later)
         if os.path.isdir(filename):
             #if folder set name and value to -1 to mark as a folder
             #call the function again with the value set to 1 to skip initial checks
@@ -87,18 +96,35 @@ def dSearch(filedir, fsave):
 
 
 #fucntion to compare files
-def dCompare(firstfile, secondfile):
-    test = 5
+def dCompare(newfile, oldfile):
+    print "in dCompare"
 
-    #TODO
-    #create a new file to set compared data to
+    #create a file to set compared data to
+    print("results being saved to Results.txt\n")
+    if os.path.exists("Results.txt"):
+        print("file results already exists overwriting\n")
+        f = file("Results.txt", "r+")
+    else:
+        f = file("Results.txt", "w")
+
+    newfilelist = []
+    oldfilelist = []
+    #grab both files into an array
+    for line in newfile:
+        newfilelist.append(line)
+    for line in oldfile:
+        oldfilelist.append(line)
+        
     #run the old file to the new file and check for differences
-    #to make sure no files are missing run through the old file
-    #through the new file
+    if newfilelist == oldfilelist:
+        print "files are the same"
+    else:
+        print "files are different"
+
+#TODO add a gui
+#TODO maybe display where files are different
+
     
-
-
-
 #standard to call main function 
 if __name__ == '__main__':
     main()
